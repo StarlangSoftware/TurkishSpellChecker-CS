a2fe09d3d787d83a0f1a1e607383fa9a812c697e
barbaros-durusoy <barbaros.durusoy@gmail.com>
2023-03-23 16:13:05 +0300

    Added new parameters: domain, minWordLength

--- SpellChecker/SpellCheckerParameter.cs
+++ SpellChecker/SpellCheckerParameter.cs
@@ -5,30 +5,110 @@
         private double _threshold = 0.0;
         private bool _deMiCheck = true;
         private bool _rootNGram = true;
+        private int _minWordLength = 4;
+        private string _domain = null;
         
+        /// <summary>
+        /// Constructs a SpellCheckerParameter object with default values.
+        /// The default threshold is 0.0, the De-Mi check is enabled, the root ngram is enabled,
+        /// the minimum word length is 4, and domain name value is null.
+        /// </summary>
+        public SpellCheckerParameter()
+        {
+        }
+        
+        /// <summary>
+        /// Sets the threshold value used in calculating the n-gram probabilities.
+        /// </summary>
+        /// <param name="threshold">the threshold for the spell checker</param>
         public void SetThreshold(double threshold)
         {
             _threshold = threshold;
         }
+        
+        /// <summary>
+        /// Enables or disables De-Mi check for the spell checker.
+        /// </summary>
+        /// <seealso cref="SimpleSpellChecker.ForcedDeDaSplitCheck(Word, Sentence)"/>
+        /// <seealso cref="SimpleSpellChecker.ForcedQuestionSuffixSplitCheck(Word, Sentence)"/>
+        /// <param name="deMiCheck">a boolean indicating whether the De-Mi check should be enabled (true) or disabled (false)</param>
         public void SetDeMiCheck(bool deMiCheck)
         {
             _deMiCheck = deMiCheck;
         }
+        
+        /// <summary>
+        /// Enables or disables the root n-gram for the spell checker.
+        /// </summary>
+        /// <param name="rootNGram">a boolean indicating whether the root n-gram should be enabled (true) or disabled (false)</param>
         public void SetRootNGram(bool rootNGram)
         {
             _rootNGram = rootNGram;
         }
+        
+        /// <summary>
+        /// Sets the minimum length of words viable for spell checking.
+        /// </summary>
+        /// <param name="minWordLength">the minimum word length for the spell checker</param>
+        public void SetMinWordLength(int minWordLength)
+        {
+            _minWordLength = minWordLength;
+        }
+        
+        /// <summary>
+        /// Sets the domain name to the specified value.
+        /// </summary>
+        /// <param name="domain">the new domain name to set for this object</param>
+        public void SetDomain(string domain)
+        {
+            _domain = domain;
+        }
+        
+        /// <summary>
+        /// Returns the threshold value used in calculating the n-gram probabilities.
+        /// </summary>
+        /// <returns>the threshold for the spell checker</returns>
         public double GetThreshold()
         {
             return _threshold;
         }
+        
+        /// <summary>
+        /// Returns whether De-Mi check is enabled for the spell checker.
+        /// </summary>
+        /// <seealso cref="SimpleSpellChecker.ForcedDeDaSplitCheck(Word, Sentence)"/>
+        /// <seealso cref="SimpleSpellChecker.ForcedQuestionSuffixSplitCheck(Word, Sentence)"/>
+        /// <returns>a boolean indicating whether De-Mi check is enabled for the spell checker</returns>
         public bool DeMiCheck()
         {
             return _deMiCheck;
         }
+        
+        /// <summary>
+        /// Returns whether the root n-gram is enabled for the spell checker.
+        /// </summary>
+        /// <returns>a boolean indicating whether the root n-gram is enabled for the spell checker</returns>
         public bool IsRootNGram()
         {
             return _rootNGram;
+        }
+        
+        /// <summary>
+        /// Returns the minimum length of words viable for spell checking.
+        /// </summary>
+        /// <returns>the minimum word length for the spell checker</returns>
+        public int GetMinWordLength()
+        {
+            return _minWordLength;
+        }
+        
+        /// <summary>
+        /// Returns the domain name.
+        /// </summary>
+        /// <returns>the domain name</returns>
+        public string GetDomain()
+        {
+            return _domain;
         }
     }
     
