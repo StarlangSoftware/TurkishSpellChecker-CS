598d81d7c38c8ad79d4b1688109906e03dec5c24
barbaros-durusoy <barbaros.durusoy@gmail.com>
2023-03-23 16:09:15 +0300

    Added documentation

--- SpellChecker/Operator.cs
+++ SpellChecker/Operator.cs
@@ -1,7 +1,48 @@
 namespace SpellChecker
 {
+    /// <summary>
+    /// Enum class that represents different types of spell checking operators that can be applied to a Word.
+    /// </summary>
     public enum Operator
     {
-        NO_CHANGE, MISSPELLED_REPLACE, FORCED_MERGE, FORCED_SPLIT, SPLIT_WITH_SHORTCUT, SPELL_CHECK, SPLIT, FORWARD_MERGE, BACKWARD_MERGE
+        /// <summary>
+        /// No change is made to the Word.
+        /// </summary>
+        NO_CHANGE, 
+        
+        /// <summary>
+        /// The Word is changed into a Word in the misspellings list.
+        /// </summary>
+        MISSPELLED_REPLACE, 
+        
+        /// <summary>
+        /// The Word is changed into a Candidate by deleting, adding, replacing a character or swapping two consecutive characters.
+        /// </summary>
+        SPELL_CHECK,
+        
+        /// <summary>
+        /// The Word is split into multiple Candidates.
+        /// </summary>
+        SPLIT, 
+        
+        /// <summary>
+        /// The Word and the Word after are merged into one Candidate.
+        /// </summary>
+        FORWARD_MERGE, 
+        
+        /// <summary>
+        /// The Word and the Word before are merged into one Candidate.
+        /// </summary>
+        BACKWARD_MERGE, 
+        
+        /// <summary>
+        /// The Word is changed into a Candidate based on the context-based spell checking algorithm.
+        /// </summary>
+        CONTEXT_BASED, 
+        
+        /// <summary>
+        /// The Word is changed into a Candidate based on the trie-based spell checking algorithm.
+        /// </summary>
+        TRIE_BASED
     }
 }
